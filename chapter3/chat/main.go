af2a10705059c75480f3df7c9bd95887c301c309
Anand Dhillon <anand@Anands-Mac-Pro.local>
2015-07-06 18:35:25 -0400

    started chapter 3

---
+++ chapter3/chat/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"go-blueprints/chapter2/trace"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+//server the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
+
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application")
+	flag.Parse()
+
+	gomniauth.SetSecurityKey("wetre94541gd616fd1g6fd")
+	gomniauth.WithProviders(
+		facebook.New("1578364069092723", "3be3e79ef1c5a14a8d556d45ab28bc23",
+			"http://localhost:8080/auth/callback/facebook"),
+		google.New("773345955621-aj0hpvne7depi9er2cp72t2mrknb3l26.apps.googleusercontent.com", "zT2_36o-f18VxmmvdltT_9vX",
+			"http://localhost:8080/auth/callback/google"),
+	)
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHander)
+	http.Handle("/room", r)
+
+	//start the room in the background
+	go r.run()
+
+	//start the web server
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
