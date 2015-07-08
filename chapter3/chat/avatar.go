c4db27e0481054b2f8714b0fcc9983a559e2699b
Anand Dhillon <anand@Anands-Mac-Pro.local>
2015-07-07 20:15:53 -0400

    working avatar upload

---
+++ chapter3/chat/avatar.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"errors"
+	"io/ioutil"
+	"path"
+)
+import gomniauthcommon "github.com/stretchr/gomniauth/common"
+
+type ChatUser interface {
+	UniqueID() string
+	AvatarURL() string
+}
+
+type chatUser struct {
+	gomniauthcommon.User
+	uniqueID string
+}
+
+func (u chatUser) UniqueID() string {
+	return u.uniqueID
+}
+
+var ErrNoAvatarURL = errors.New("chat: Unable to get an avatar URL.")
+
+type Avatar interface {
+	//gets the url for the specified client or returns an error
+	GetAvatarURL(u ChatUser) (string, error)
+}
+
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	url := u.AvatarURL()
+	if len(url) > 0 {
+		return url, nil
+	}
+	return "", ErrNoAvatarURL
+}
+
+type GravatarAvatar struct{}
+
+var UseGravatar GravatarAvatar
+
+func (_ GravatarAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	return "//www.gravatar.com/avatar/" + u.UniqueID(), nil
+}
+
+type FileSystemAvatar struct{}
+
+var UseFileSystemAvatar FileSystemAvatar
+
+func (_ FileSystemAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	if files, err := ioutil.ReadDir("avatars"); err == nil {
+		for _, file := range files {
+			if file.IsDir() {
+				continue
+			}
+			if match, _ := path.Match(u.UniqueID()+"*", file.Name()); match {
+				return "/avatars/" + file.Name(), nil
+			}
+		}
+	}
+
+	return "", ErrNoAvatarURL
+
+}
